503838c700816a67fad4cecb5ca559ba683adc66
Janice-M <janiceink001@gmail.com>
2020-10-08 23:33:00 +0300

    class unauthenticated button

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -9,3 +9,11 @@ import {
 
 import { AccountCircle } from '@material-ui/icons';
 import { withAuth } from '@okta/okta-react';
+
+
+class LoginButton extends Component {
+    state = {
+      authenticated: null,
+      user: null,
+      menuAnchorEl: null,
+    };
