681b3cd63c6e84f854856df96abce506c00a7744
Janice-M <janiceink001@gmail.com>
2020-10-08 23:16:30 +0300

    ockta security

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Security } from '@okta/okta-react';
 
 ReactDOM.render(
   <React.StrictMode>
