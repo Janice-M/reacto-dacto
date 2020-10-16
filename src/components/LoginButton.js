21efe07f8f983435c8aec61bfdf597ef1f6f130d
Janice-M <janiceink001@gmail.com>
2020-10-16 23:57:07 +0300

    sqlite

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -39,7 +39,7 @@ class LoginButton extends Component {
     }
 
     handleMenuOpen = event => this.setState({ menuAnchorEl: event.currentTarget });
-    handleMenuClose = () => this.sette({ menuAnchorEl: null });
+    handleMenuClose = () => this.setState({ menuAnchorEl: null });
 
 
     render() {
